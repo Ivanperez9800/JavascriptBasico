3cca2cff066b181a95d448dde9de62d1007c5941
ivan perez <ivan_perez0980@proton.me>
2023-08-14 15:49:31 -0300

    Acceso por indice e arrays anidados

--- Arrays.js
+++ Arrays.js
@@ -1,1 +1,18 @@
 //ARRAYS
+//ARRAYS: CONJUNTO DE DATOS DEL MISMO O DIFERENTES TIPOS
+const sandwich = ["peanut butter", "jelly", "bread"]; //ARRAYS DE STRINGS
+const precios = [25,70,50,300]; //ARRAY DE ENTEROS
+const sanguchitos =["Jelly",50,"Jamon",300]; //ARRAY DE ENTERO Y STRINGS
+
+//PARA OBTENER EL VALOR DE UN ARRAY SE UTILIZAN LOS INDICES, ESTOS EMPIEZAN EN 0
+
+console.log(precios[2]); //50
+
+//LOS ARRAYS TAMBIEN PUEDEN SER ANIDADOS
+//LOS INDICES EN LOS ANIDADOS SON COMO FILAS Y COLUMNAS, FILA1 COLUMNA1, FILA2,COLUMNA1....
+                  
+
+                    //0                //1
+                //0   //1         //0        //1       
+const myArray = [["Bulls", 23], ["White Sox", 45]];
+
